00524a8eb4ac4c4c9349e7d193bc2666b94fe286
Yacoub Harkati <yaharkat@student.1337.ma>
2023-11-16 18:20:04 +0100

    added bonus and fixed minor issues

---
+++ get_next_line_bonus.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaharkat <yaharkat@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 21:59:16 by yaharkat          #+#    #+#             */
+/*   Updated: 2023/11/16 02:12:46 by yaharkat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*get_clean_line(char *buffer)
+{
+	int	i;
+
+	if (!buffer || !*buffer)
+		return (NULL);
+	i = 0;
+	while (buffer[i] != '\n' && buffer[i] != '\0')
+		i++;
+	return (ft_substr(buffer, 0, i + 1));
+}
+
+char	*get_rest(char *s)
+{
+	char	*rest;
+	int		i;
+	int		len;
+
+	i = 0;
+	if (!s[i])
+	{
+		free(s);
+		return (NULL);
+	}
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (s[i] && s[i] == '\n')
+		i++;
+	len = ft_strlen(&s[i]) + 1;
+	rest = (char *)malloc(sizeof(char) * len);
+	ft_strlcpy(rest, s + i, len);
+	free(s);
+	return (rest);
+}
+
+char	*initisalize_buffer(void)
+{
+	char	*buffer;
+
+	if (BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	buffer[0] = '\0';
+	return (buffer);
+}
+
+char	*read_line_lines(char *buffer, char *line, int fd, ssize_t *bytes_read)
+{
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	*bytes_read = read(fd, line, BUFFER_SIZE);
+	if (*bytes_read < 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	line[*bytes_read] = '\0';
+	buffer = ft_strjoin(buffer, line);
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line[2];
+	static char	*buffer[1024];
+	ssize_t		bytes_read;
+
+	line[0] = initisalize_buffer();
+	if (line[0] == NULL)
+		return (NULL);
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, line[0], 0) < 0)
+	{
+		free(line[0]);
+		return (NULL);
+	}
+	bytes_read = 1;
+	while (bytes_read > 0 && !ft_strchr(line[0], '\n'))
+		buffer[fd] = read_line_lines(buffer[fd], line[0], fd, &bytes_read);
+	line[1] = get_clean_line(buffer[fd]);
+	buffer[fd] = get_rest(buffer[fd]);
+	free(line[0]);
+	return (line[1]);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaharkat <yaharkat@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 21:59:21 by yaharkat          #+#    #+#             */
+/*   Updated: 2023/11/16 02:13:02 by yaharkat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)(s + i));
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*dst;
+	size_t	size;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+	{
+		s1 = (char *)malloc(sizeof(char));
+		if (!s1)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	size = ft_strlen(s1) + ft_strlen(s2);
+	dst = (char *)malloc(sizeof(char) * (size + 1));
+	if (!dst)
+		return (NULL);
+	ft_strlcpy(dst, s1, (size + 1));
+	ft_strlcpy((dst + ft_strlen(s1)), s2, (size + 1));
+	free(s1);
+	return (dst);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		sub = (char *)malloc(sizeof(char));
+		if (!sub)
+			return (NULL);
+		sub[0] = '\0';
+		return (sub);
+	}
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, &s[start], (len + 1));
+	return (sub);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size > 0 && dst && src)
+	{
+		while (src[i] && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
